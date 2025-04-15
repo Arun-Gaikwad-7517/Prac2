6308d45d5687d0aba1047d7adbd38945247bbe55
ankush-diwakar <150348342+ankush-diwakar@users.noreply.github.com>
2025-04-15 21:37:09 +0530

    setup intial project and add loader component

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Loader from "./components/Loader";
+
+const App = () => {
+  return(
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <Loader />
+    </div>
+  )
+}
+
+export default App;
