69578053accdb1f454ed43897524bc709d38fb1b
sarang <gaikwadarun7517@gmail.com>
2025-04-15 22:01:58 +0530

    add menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import Loader from "./components/Loader";
+import { Menu } from "./components/Menu";
+
+
 
 const App = () => {
   return(
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
       <Loader />
+      <Menu/>
     </div>
   )
 }
+
 
 export default App;
